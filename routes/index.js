75c79a248886689829e96c19e349cccad3e69d51
ed <elennon@outlook.ie>
2016-10-18 22:02:44 +0100

    added highcharts

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient
+var url = 'mongodb://localhost:27017/Measurements';	
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+    var results_from_mongo = [];
+    var temps = [];
+    var afrr = [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8];
+
+    MongoClient.connect(url, function (err, db) {
+        var str = db.collection('Bmp180').find({}, {'temp' : true}).toArray(function(err, results_from_mongo) {
+        	for ( index in results_from_mongo){
+				var doc = results_from_mongo[index];
+				
+				var temp = doc['temp'];
+				
+				temps.push(temp);
+		    }
+	    	console.log(afrr);
+        	res.render('index', {"results": afrr });
+		});  
+    });  
 });
 
 module.exports = router;
