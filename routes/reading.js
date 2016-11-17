03b5be940d36e97544703cf2ccac88dd54a7412e
ed <elennon@outlook.ie>
2016-11-17 00:03:56 +0000

    r t m

--- routes/reading.js
+++ routes/reading.js
@@ -22,7 +22,7 @@ router.get('/time', function(req, res) {
 			if (err) throw err;
 			console.log('%j', results);
 			
-			var a = new Date(results[0].createdAt * 1000);
+			var a = new Date(results[0].createdAt);
 			var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
 			var year = a.getFullYear();
 			var month = months[a.getMonth()];
